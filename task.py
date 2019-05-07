4f1c40dd779b694469035adc10d63f43f243cc20
Michał Piwowarczyk <micpiw16@gmail.com>
2019-05-07 15:16:56 +0200

    improved version

--- task.py
+++ task.py
@@ -1,32 +1,55 @@
 from random import gauss
 import time
+import logging
 
-class FlightSimulator(object):
+logging.basicConfig(level=logging.DEBUG)
+
+class Environment(object):
 	def __init__(self):
-		self.tilt_angle = 0.0
+		self.planes_in_env = [] 
 
-	#def turbulence(self):
-	#	self.tilt_angle = gauss(0.0, 15.0)
+	def add_plane(self, plane):
+		self.planes_in_env.append(plane)
 
-	def run_simulation(self):
-		while 1:
-			#self.turbulence()
-			self.__next__()
-			print("Huston we have a problem!!!!!")
-			if self.tilt_angle > 0.0:
-				print("turn", abs(int(self.tilt_angle)), "degres right")
+	def simulate_trbulance(self):
+		turbulance_value = gauss(0.75, 1.0)
+		for j,i in enumerate(self.planes_in_env):
+			if i.is_alive:
+				i.tilt_sensor(turbulance_value)
+				i.corect_angle()
 			else:
-				print("turn", abs(int(self.tilt_angle)), "degres left")
+				self.planes_in_env.pop(j)
+		time.sleep(2)
 
-			time.sleep(2)
+
+class Plane(object):
+	tilt_correction = 0.75
+	def __init__(self, name):
+		self.tilt_angle = gauss(0,4)
+		self.name = name
+		self.is_alive = True
 	
-	def __iter__(self):
-		return self
+	def tilt_sensor(self, turbulance_value):
+		self.tilt_angle += turbulance_value
 
-	def __next__(self):
-		self.tilt_angle = gauss(0.0, 15.0)
-		
-
-if __name__ == "__main__":
-	simulator = FlightSimulator()
-	simulator.run_simulation()
+	def corect_angle(self):
+		logging.debug('It\'s {} plane'.format(self.name))
+		logging.debug('Our tilt angle is {}'.format(self.tilt_angle))
+		if self.tilt_angle > 0.0:
+			self.tilt_angle -= 0.75
+			logging.debug('turning {} degres left'.format(self.tilt_correction))
+		else:
+			self.tilt_angle += 0.75
+			logging.debug('turning {} degres right'.format(self.tilt_correction))
+		if abs(self.tilt_angle) > 90:
+			logging.debug('This is the end :(')
+			self.is_alive = False
+			
+if __name__ == '__main__':
+	f16_plane = Plane("F16")
+	boeing = Plane("boeing")
+	envir = Environment()
+	envir.add_plane(f16_plane)
+	envir.add_plane(boeing)
+	while True:
+		envir.simulate_trbulance()
